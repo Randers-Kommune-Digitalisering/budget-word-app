77a25397910f783e022620b26b91d1303b945ac6
kf-randers <kfkr@randers.dk>
2023-08-23 15:14:49 +0200

    Første forsøg

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -20,7 +20,7 @@ export async function run() {
      */
 
     // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Hello World", Word.InsertLocation.end);
+    const paragraph = context.document.body.insertParagraph("Det virker sørme...., Word.InsertLocation.end);
 
     // change the paragraph color to blue.
     paragraph.font.color = "blue";
