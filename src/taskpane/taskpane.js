7b248232eb9484179c810fd8bf68c89bc4a28e30
kf-randers <kfkr@randers.dk>
2023-08-23 15:16:36 +0200

    mindre rettelse

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -20,7 +20,7 @@ export async function run() {
      */
 
     // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Det virker sørme...., Word.InsertLocation.end);
+    const paragraph = context.document.body.insertParagraph("Det virker sørme....", Word.InsertLocation.end);
 
     // change the paragraph color to blue.
     paragraph.font.color = "blue";
