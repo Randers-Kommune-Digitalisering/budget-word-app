a2e57cd7bf79bcdf8341ecb0fbfd67ed5d876c3c
kf-randers <kfkr@randers.dk>
2023-08-24 13:50:20 +0200

    Tilføjet organisation

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -19,11 +19,68 @@ export async function run() {
      * Insert your Word code here
      */
 
+    const organisation =
+    [
+        {
+            "udvalg":"Økonomiudvalget",
+            "bevillingsomr":["Administration","Politisk organisation"]
+        },
+        {
+            "udvalg":"Børne- og familieudvalget",
+            "bevillingsomr":["Børn","Familie"]
+        },
+        {
+            "udvalg":"Beskæftigelsesudvalget",
+            "bevillingsomr":["Beskæftigelse, integration og ydelser"]
+        },
+        {
+            "udvalg":"Socialudvalget",
+            "bevillingsomr":["Tilbud til børn, unge og voksne med særlige behov"]
+        },
+        {
+            "udvalg":"Miljø- og teknikudvalget",
+            "bevillingsomr":["Miljø og teknik, skattefinansieret","Miljø og teknik, takstfinansieret"]
+        },
+        {
+            "udvalg":"Sundheds-, idræts- og kulturudvalget",
+            "bevillingsomr":["Kultur og fritid","Sundhed"]
+        },
+        {
+            "udvalg":"Omsorgsudvalget",
+            "bevillingsomr":["Tilbud til ældre"]
+        },
+        {
+            "udvalg":"Landdistriktsudvalget",
+            "bevillingsomr":["Landdistrikt"]
+        },
+        {
+            "udvalg":"Skole- og uddannelsesudvalget",
+            "bevillingsomr":["Skole","Pædagogisk Psykologisk Rådgivning"]
+        },
+        {
+            "udvalg":"Erhvervs- og planudvalget",
+            "bevillingsomr":["Erhverv og plan"]
+        }
+    ]
+
     // insert a paragraph at the end of the document.
-    const paragraph = context.document.body.insertParagraph("Det virker sørme....", Word.InsertLocation.end);
+
+    for (var key in organisation) {
+      if (organisation.hasOwnProperty(key)) {
+        for (var key2 in organisation[key].bevillingsomr) {
+          if (organisation[key].bevillingsomr.hasOwnProperty(key2)) {
+
+            const tekst=organisation[key].udvalg + " - " + organisation[key].bevillingsomr[key2]
+            context.document.body.insertParagraph(tekst, Word.InsertLocation.end); 
+          }
+        }
+      }
+    }
+
+      //const paragraph2 = context.document.body.insertParagraph(organisation[1].udvalg, Word.InsertLocation.end);
 
     // change the paragraph color to blue.
-    paragraph.font.color = "blue";
+    // paragraph.font.color = "blue";
 
     await context.sync();
   });
