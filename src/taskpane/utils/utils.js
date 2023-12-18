9bc01d295cd9ada29a365665e9c5e1ba4a8bf30f
kf-randers <kfkr@randers.dk>
2023-12-18 15:52:46 +0100

    Nyt UI og ongoing refaktorering

---
+++ src/taskpane/utils/utils.js
@@ -0,0 +1,83 @@
+// Formatter tabeller
+export async function formaterTabeller(){
+    return Word.run(async (context) => {
+        
+      
+
+      // Loop over alle tabeller
+      var tables=context.document.body.tables
+      tables.load('items')
+      await context.sync()
+      if (tables.items.length > 0) {
+        for (var j=0;j<tables.items.length;j++) {
+          var table = tables.items[j];
+          table.font.bold=false
+          table.font.size=8
+  
+          // Fjerner alle rammer
+          var borderLocation = Word.BorderLocation.all;
+          var border = table.getBorder(borderLocation);
+          border.set({type:'none'})
+  
+          // Tilføjer horisontale streger
+          var borderLocation = Word.BorderLocation.insideHorizontal
+          var border = table.getBorder(borderLocation);
+          border.set({color:"#D9D9D9",width:1, type:'Single'})
+  
+          // Loop over alle rækker
+          var rækker=table.rows
+          rækker.load('items')
+          await context.sync()
+          for (var i=0; i<rækker.items.length; i++) {
+            rækker.items[i].verticalAlignment="Center"
+            
+            // Styler førster og sidste række
+            if (i==0|i==rækker.items.length-1) {
+              var borderLocation = Word.BorderLocation.top;
+              var border = rækker.items[i].getBorder(borderLocation);
+              border.set({color:"#808080",width:1, type:'Single'})
+              var borderLocation = Word.BorderLocation.bottom;
+              var border = rækker.items[i].getBorder(borderLocation);
+              border.set({color:"#808080",width:1, type:'Single'})
+              rækker.items[i].shadingColor="#DDEBF7"
+              rækker.items[i].font.bold=true
+            }
+  
+            // Loop over celler
+            var celler=rækker.items[i].cells
+            celler.load('items')
+            await context.sync()
+            for (var k=0; k<celler.items.length; k++) {
+              // Sætter padding
+              celler.items[k].setCellPadding("Top",3.4)
+              celler.items[k].setCellPadding("Bottom",3.4)
+  
+              // Højrestiller kolonne > 1 og række > 1
+              if (i>0&k>0) {
+                celler.items[k].horizontalAlignment="Right" 
+              } 
+              if (k==0) {
+                celler.items[k].columnWidth=250
+              }
+              if (celler.items[k].value.slice(0,5)=="I alt") {
+                console.log(celler.items[k].value.slice(0,5))
+                rækker.items[i].shadingColor="#DDEBF7"
+                rækker.items[i].font.bold=true
+                celler.items[k].setCellPadding("Top",3.4)
+                celler.items[k].setCellPadding("Bottom",3.4)
+                var borderLocation = Word.BorderLocation.top;
+                var border = rækker.items[i].getBorder(borderLocation);
+                border.set({color:"#808080",width:1, type:'Single'})
+                var borderLocation = Word.BorderLocation.bottom;
+                var border = rækker.items[i].getBorder(borderLocation);
+                border.set({color:"#808080",width:1, type:'Single'})
+              }
+  
+            }
+          }
+        }
+        await context.sync()
+        }
+      await context.sync()
+    })
+  }
