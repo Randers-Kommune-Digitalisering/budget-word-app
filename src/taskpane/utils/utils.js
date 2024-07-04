dbc1d2ddd35801bc6a67829481fb506cf28759ca
Rune Aagaard Keena <rune.aagaard.keena@randers.dk>
2024-07-04 08:42:57 +0200

    HACKY - insert data into template from excel file

--- src/taskpane/utils/utils.js
+++ src/taskpane/utils/utils.js
@@ -1,3 +1,9 @@
+export function sumArrays(...arrays) {
+  const n = arrays.reduce((max, xs) => Math.max(max, xs.length), 0);
+  const result = Array.from({ length: n });
+  return result.map((_, i) => arrays.map(xs => xs[i] || 0).reduce((sum, x) => sum + x, 0));
+}
+
 // Formatter tabeller
 export async function formaterTabeller(){
     return Word.run(async (context) => {
