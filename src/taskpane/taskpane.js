c953a5d06783b85abc97efe0530246de2491eb32
Karl Fritjof Krassel <122357806+kf-randers@users.noreply.github.com>
2024-11-07 10:31:40 +0100

    fix

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -3,7 +3,7 @@
 /* eslint-disable no-prototype-builtins */
 /* eslint-disable no-undef */
 //import { ContextExclusionPlugin } from "webpack";
-import { formaterTabeller, formaterTabellerBB, sumArrays } from "./utils/utils.js";
+import { formaterTabeller, formaterTabellerBB, formatSelectedTable, formatIntermediateSumRow, sumArrays } from "./utils/utils.js";
 import { generateTable, readFile } from "./utils/data.js";
 
 const required_styles = ["Brev/notat KORT (O1)"];
@@ -19,7 +19,8 @@ Office.onReady((info) => {
     document.getElementById("rydAltTools").onclick = () => tryCatch(rydAlt);
     document.getElementById("rydAltDev").onclick = () => tryCatch(rydAlt);
     document.getElementById("formaterTabellerBO").onclick = () => tryCatch(formaterTabeller);
-    document.getElementById("formaterTabellerBB").onclick = () => tryCatch(formaterTabellerBB);
+    document.getElementById("formatSelectedTable").onclick = () => tryCatch(formatSelectedTable);
+    document.getElementById("formatIntermediateSumRow").onclick = () => tryCatch(formatIntermediateSumRow);
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
 
--- src/taskpane/utils/utils.js
+++ src/taskpane/utils/utils.js
@@ -5,6 +5,161 @@ export function sumArrays(...arrays) {
   return result.map((_, i) => arrays.map((xs) => xs[i] || 0).reduce((sum, x) => sum + x, 0));
 }
 
+export async function formatIntermediateSumRow() {
+  return Word.run(async (context) => {
+    const selection = context.document.getSelection();
+    selection.load("parentTable, parentTableCell");
+    await context.sync();
+
+    const rowToEdit = selection.parentTableCell._R
+    const table = selection.parentTable;  
+    
+    const rows = table.rows;
+    rows.load("items");
+    await context.sync();
+
+    for (var i = 0; i < rows.items.length; i++) {
+      if (i == rowToEdit) {
+        var borderLocation = Word.BorderLocation.top;
+        var border = rows.items[i].getBorder(borderLocation);
+        border.set({ color: "#808080", width: 1, type: "Single" });
+        var borderLocation = Word.BorderLocation.bottom;
+        var border = rows.items[i].getBorder(borderLocation);
+        border.set({ color: "#808080", width: 1, type: "Single" });
+        rows.items[i].shadingColor = "#DDEBF7";
+        rows.items[i].font.bold = true;
+        rows.items[i].font.name = "Calibri";
+      }
+    }
+    
+  });
+}
+
+
+export async function formatSelectedTable() {
+  return Word.run(async (context) => {
+    const selection = context.document.getSelection();
+    selection.load("parentTable");
+    await context.sync();
+
+    const table = selection.parentTable;  
+
+    table.headerRowCount = 1;
+    table.font.bold = false;
+    table.font.size = 9;
+
+    // Fjerner alle rammer
+    var borderLocation = Word.BorderLocation.all;
+    var border = table.getBorder(borderLocation);
+    await context.sync();
+    border.set({ type: "none" });
+
+    // Tilføjer horisontale streger
+    var borderLocation = Word.BorderLocation.insideHorizontal;
+    var border = table.getBorder(borderLocation);
+    await context.sync();
+    border.set({ color: "#D9D9D9", width: 0.5, type: "Single" });
+
+    // Loop over alle rækker
+    const rækker = table.rows;
+    rækker.load("items");
+    await context.sync();
+
+    for (var i = 0; i < rækker.items.length; i++) {
+      rækker.items[i].verticalAlignment = "Center";
+
+      // Styler første og sidste række
+      if ((i == 0) | (i == rækker.items.length - 1)) {
+        var borderLocation = Word.BorderLocation.top;
+        var border = rækker.items[i].getBorder(borderLocation);
+        border.set({ color: "#808080", width: 1, type: "Single" });
+        var borderLocation = Word.BorderLocation.bottom;
+        var border = rækker.items[i].getBorder(borderLocation);
+        border.set({ color: "#808080", width: 1, type: "Single" });
+        rækker.items[i].shadingColor = "#DDEBF7";
+        rækker.items[i].font.bold = true;
+        rækker.items[i].font.name = "Calibri";
+      }
+
+
+
+      // Loop over celler
+      var celler = rækker.items[i].cells;
+      celler.load("items");
+      await context.sync();
+
+      const sidebredde = 468;
+      const tabelBredde = sidebredde;
+      var venstreKolonne = 0.4 
+      if (celler.items.length > 3) {
+        venstreKolonne = 0.3;
+      }
+      
+      table.width = sidebredde;
+
+      for (var k = 0; k < celler.items.length; k++) {
+     
+        // Instiller kolonnebredde
+        if (k == 0) {
+          celler.items[k].columnWidth = tabelBredde * venstreKolonne;
+        }
+        if (k >= 1) {
+          celler.items[k].columnWidth = tabelBredde * ((1-venstreKolonne)/(celler.items.length-1));
+        }
+
+        // Sætter padding
+        if (i == 0) {
+          celler.items[k].setCellPadding("Top", 10);
+          celler.items[k].setCellPadding("Bottom", 10);
+        } else  {
+         
+          celler.items[k].setCellPadding("Top", 2);
+          celler.items[k].setCellPadding("Bottom", 2);
+        }
+         
+        // Tekstfarve
+        celler.items[k].body.font.set({color: "#000000"});
+
+        // Højrestiller kolonneoverskrifter til højre hvis kun tal -
+        // OBS: Redundant - gælder åbenbart ikke ikke BB-tabeller!
+        if ((i == 0) & (k > 0)) {
+          const årstal = /^\d{4}$/;
+          if (årstal.test(celler.items[k].value) == true) {
+            celler.items[k].horizontalAlignment = "Centered";
+          } else {
+            celler.items[k].horizontalAlignment = "Centered";
+          }
+        }
+        // Højrestiller kolonne > 1 og række > 1
+        if ((i > 0) & (k > 0)) {
+          celler.items[k].horizontalAlignment = "Right";
+        }
+        // if ((k == 0) & (i > 0) & (i < rækker.items.length - 1)) {
+        //  celler.items[k].setCellPadding("Left", 10);
+        // }
+        // Styler i alt-rækker
+        if (celler.items[k].value.slice(0, 5) == "I alt") {
+          console.log(celler.items[k].value.slice(0, 5));
+          rækker.items[i].shadingColor = "#DDEBF7";
+          rækker.items[i].font.bold = true;
+          celler.items[k].setCellPadding("Top", 3);
+          celler.items[k].setCellPadding("Bottom", 3);
+          var borderLocation = Word.BorderLocation.top;
+          var border = rækker.items[i].getBorder(borderLocation);
+          border.set({ color: "#808080", width: 1, type: "Single" });
+          var borderLocation = Word.BorderLocation.bottom;
+          var border = rækker.items[i].getBorder(borderLocation);
+          border.set({ color: "#808080", width: 1, type: "Single" });
+        }
+      }
+      await context.sync();
+    }
+    await context.sync();
+  });
+}
+
+
+
 // Formatter tabeller
 export async function formaterTabeller() {
   return Word.run(async (context) => {
